011853978b158006a0c8c21300447040811a3eb0
chenyirex <chenyirex@outlook.com>
2018-10-30 15:25:26 -0700

    first commit

---
+++ src/main/java/com/ubc/cpsc304/hotelify/HotelifyApplication.java
@@ -0,0 +1,12 @@
+package com.ubc.cpsc304.hotelify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelifyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ubc/cpsc304/hotelify/HotelifyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ubc.cpsc304.hotelify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelifyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
