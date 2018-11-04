4371a505cc1c676bfe6db5ea530f155be67002f0
suiyoubi <mike.tang96@gmail.com>
2018-11-04 15:39:23 -0800

    Enable CORS

---
+++ src/main/java/com/ubc/cpsc304/hotelify/HotelifyConfiguration.java
@@ -0,0 +1,19 @@
+package com.ubc.cpsc304.hotelify;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Customized Configurations.
+ * Created by ao on 2018-11-04
+ */
+@Configuration
+public class HotelifyConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
