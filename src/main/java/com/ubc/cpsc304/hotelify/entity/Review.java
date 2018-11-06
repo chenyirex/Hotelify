58597af78ecf6fe5de485c307a1b7293d9483abb
klQiu <elviselvislivbos@gmail.com>
2018-11-06 13:31:09 -0800

    fixes annotations

--- src/main/java/com/ubc/cpsc304/hotelify/entity/Review.java
+++ src/main/java/com/ubc/cpsc304/hotelify/entity/Review.java
@@ -3,25 +3,29 @@ package com.ubc.cpsc304.hotelify.entity;
 import javax.persistence.*;
 
 import lombok.Data;
+import lombok.Generated;
 
 @Data
 @Entity
 public class Review {
 
     @Id
+    @Generated
     @Column(nullable = false, updatable = false, unique = true)
     private Long review_id;
 
+    @ManyToMany
     @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT ''")
     private String username;
 
+    @ManyToOne
     @Column(nullable = false)
     private Long hotel_id;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(255) DEFAULT ''")
     private String comment;
 
-    @Column(nullable = false)
+    @Column
     private Double rating;
 
 }
